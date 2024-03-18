52cc8637a88859b9b1cd2ea3741bffa63754d037
wnguscjf01 <wnguscjf01@gmail.com>
2024-03-18 14:53:33 +0900

    Add add-nbo.cpp

--- add-nbo.cpp
+++ add-nbo.cpp
@@ -3,6 +3,14 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 
+int getFileSize(FILE* file)
+{
+    int currentPosition = ftell(file);
+    fseek(file, 0, SEEK_END);
+    int fileSize = ftell(file);
+    fseek(file, currentPosition, SEEK_SET);
+    return fileSize;
+}
 int main(int argc, char* argv[]) {
 
     uint32_t x, y;
@@ -13,6 +21,16 @@ int main(int argc, char* argv[]) {
 
     f1 = fopen(argv[1], "rb");
     f2 = fopen(argv[2], "rb");
+
+    if(f1==NULL || f2==NULL){
+        fprintf(stderr,"file open errer ocurred!");
+        return 0;
+    }
+    int size1 = getFileSize(f1), size2 = getFileSize(f2);
+    if(size1<4 || size2<4){
+        fprintf(stderr,"file size below 4 bytes!");
+        return 0;
+    }
 
     fread(&x, sizeof(uint32_t), 1, f1);
     fread(&y, sizeof(uint32_t), 1, f2);
